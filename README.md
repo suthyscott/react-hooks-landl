af8e269a889a908ff1eedfb8b1dc3044027f69d0
Scott Sutherland <ssutherland@sokkas-boomerang.local>
2021-03-23 16:47:23 -0600

    removed hooks stuff

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+What are hooks? React hooks are methods that can be imported into functional components to allow use of features like state management and similar functionality to lifecycle methods.
+
+Why are hooks different than typical methods? Because hooks are built into React, allowing you to re-render the component and otherwise mimic the behavior of a class component with state.
+
+Why would you use hooks rather than classical React? The main benefit is ease of use and efficiency. Hooks are simpler to write, eliminating the need to deal with the 'this' keyword and constructor functions. Hooks generally decrease the number of lines of code in a file noticeably. Also, the React devs have forecasted performance enhancements for functional components in the future.
+
 How state is handled in class components.
 
 We use setState because it triggers a re-render, thus showing the updates.
--- src/Hooks.js
+++ src/Hooks.js
@@ -1,34 +1,6 @@
 import React, { useState, useEffect } from "react";
-import useSWAPI from "./useSWAPI";
 
 const Hooks = () => {
-  const [todos, setTodos] = useState([]);
-  const [name, setName] = useState("");
-  const [priority, setPriority] = useState(0);
-  const ship = useSWAPI();
-
-  useEffect(() => {
-    console.log("hit useEffect");
-    setTodos([
-      {
-        name: "make money",
-        priority: 7
-      },
-      {
-        name: "git gud",
-        priority: 10
-      }
-    ]);
-  }, []);
-
-  //   useEffect(() => {
-  //     console.log("hit useEffect name");
-  //   }, [name]);
-
-  //   useEffect(() => {
-  //     console.log("hit useEffect todos");
-  //   }, [todos]);
-
   return (
     <div className="container">
       <section>
@@ -43,21 +15,9 @@ const Hooks = () => {
       </section>
 
       <section className="input-container">
-        <input value={name} onChange={(e) => setName(e.target.value)} />
-        <input
-          value={priority}
-          type="number"
-          onChange={(e) => setPriority(e.target.value)}
-        />
-        <button
-          onClick={() => {
-            setTodos([...todos, { name, priority }]);
-            setName("");
-            setPriority(0);
-          }}
-        >
-          Add Item
-        </button>
+        <input value={name} />
+        <input value={priority} type="number" />
+        <button onClick={() => {}}>Add Item</button>
       </section>
     </div>
   );
